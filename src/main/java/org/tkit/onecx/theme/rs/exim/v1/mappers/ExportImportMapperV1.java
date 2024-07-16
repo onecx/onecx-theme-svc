446de8a12276a46566aa2eb999263f95f632ce83
jsteenke <146953549+jsteenke@users.noreply.github.com>
2024-07-16 06:18:14 +0200

    feat: displayName export import (#75)
    * feat: add displayName
    
    * feat: set displayname to name field
    
    * feat: add displayName exim
    
    ---------
    
    Co-authored-by: Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>

--- src/main/java/org/tkit/onecx/theme/rs/exim/v1/mappers/ExportImportMapperV1.java
+++ src/main/java/org/tkit/onecx/theme/rs/exim/v1/mappers/ExportImportMapperV1.java
@@ -104,7 +104,6 @@ public abstract class ExportImportMapperV1 {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "name", ignore = true)
-    @Mapping(target = "displayName", ignore = true)
     @Mapping(target = "creationDate", ignore = true)
     @Mapping(target = "creationUser", ignore = true)
     @Mapping(target = "modificationDate", ignore = true)
@@ -118,7 +117,6 @@ public abstract class ExportImportMapperV1 {
     public abstract void update(EximThemeDTOV1 dto, @MappingTarget Theme entity);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "displayName", ignore = true)
     @Mapping(target = "creationDate", ignore = true)
     @Mapping(target = "creationUser", ignore = true)
     @Mapping(target = "modificationDate", ignore = true)
